9564b81dcc4a81b57b4cdd9707c7bef162b2b290
admin <ekene@Ubuntu22.04.myguest.virtualbox.org>
2023-03-16 14:16:34 +0100

    C programming puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@ int main()
 {
   /*string initialisation*/
     char Mystr[] = "\"Programming is like building a multilingual puzzle";
-    puts(Mystr); //writing the string to stdout
+    puts(Mystr);
     return 0;
 }
