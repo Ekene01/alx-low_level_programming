75c2b1eecf08108fc5898b1c091c0817ffc83729
admin <ekene@Ubuntu22.04.myguest.virtualbox.org>
2023-03-17 12:28:48 +0100

    boolean operators for last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+* main - A program that prints the last digit
+* Return: 0 (Success)
+*/
 int main(void)
 {
 int n;
@@ -11,7 +12,7 @@ int last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-last_digit = n %10;
+last_digit = n % 10;
 if (last_digit > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 else
