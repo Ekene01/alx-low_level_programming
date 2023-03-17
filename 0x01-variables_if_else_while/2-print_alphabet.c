5e893435acdbfe1f084c1cdc079ec84179076a03
admin <ekene@Ubuntu22.04.myguest.virtualbox.org>
2023-03-17 12:59:23 +0100

    alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,14 +3,14 @@
 * main - A program that prints the alphabets from a-z using putchar
 * Return: 0 (Success)
 */
-int main()
+int main (void)
 {
 char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
 int i;
-for(i = 0; i < 26; i++)
+for (i = 0; i < 26; i++)
 {
-  putchar(alphabets[i]);
+putchar (alphabets[i]);
 }
-putchar('\n');
-return 0;
+putchar ('\n');
+ return (0);
 }
