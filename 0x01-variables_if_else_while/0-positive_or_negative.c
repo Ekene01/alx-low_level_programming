051b0f91f4f8cd9c11e5863128799d1946a7c09a
admin <ekene@Ubuntu22.04.myguest.virtualbox.org>
2023-03-17 12:24:11 +0100

    boolean operators for last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,20 +4,22 @@
 #include <math.h>
 
 
-/* betty style doc for function main goes there */
+/**
+ * main - Function for positive or negative
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-        printf("%d is positive\n", n);
-	else
-        if (n == 0)
-            printf("%d is zero\n", n);
-        else
-            printf("%d is negative", n);
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+printf("%d is positive", n);
+else
+if (n < 0)
+printf("%d is negative", n);
+else
+printf("%d is zero", n);
+return (0);
 }
