c62a6963e166cd8f7836159f5ad13481391b4201
admin <ekene@Ubuntu22.04.myguest.virtualbox.org>
2023-03-21 12:02:40 +0100

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - A program that prints all the alphabets ten times
+* Return: 0 (Success)
+*/
+
+void print_alphabet(void){
+int i;
+int j;
+for(j=0;j<10;j++)
+{
+for(i=97; i<=122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c~
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main - A program that prints all the alphabets
+* Return: 0 (Success)
+*/
+
+void print_alphabet(void){
+int i;
+for(i=97; i <=122; i++)
+{
+_putchar(i);
+}
+_putchar('/n');
+}
