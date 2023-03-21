e3426f29ea3b9ee8e1f7e91e725a2ccfca2d0911
admin <ekene@Ubuntu22.04.myguest.virtualbox.org>
2023-03-21 16:40:46 +0100

    printing numbers backwards and forward

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,37 +1,21 @@
 #include <stdio.h>
 #include "main.h"
-/**
-* print_to_98- Function to print all natural numbers from n to 98
-*
-* @n - starting number
-*
-* Return: Always 0
+/** 
+* print_to_98 - print all numbers from input to 98
+* @n: the starting number 
 */
-void print_to_98(int n)
+void print_to_98(void n)
+{ 
+if (n >= 98) 
 {
-int i;
-for (i = n; i <=98; i++)
-{
-if(i != 98)
-{
-printf("%d", i);
-_putchar(44);
-_putchar(32);
-}
+while (n > 98)
+printf ("%d, ", n--) 
+printf("%d\n", n);
+} 
 else
 {
-printf("%d", i);
-}
-}
-for (i = n; i >=98; i--)
-{
-if(i != 98){
-printf("%d", i);
-_putchar(44);
-_putchar(32);
-}
-else {
-printf("%d", i);
-}
-}
-}
+while (n < 98)
+printf("%d,", n++);
+printf("%d\n", n);
+} 
+} 
