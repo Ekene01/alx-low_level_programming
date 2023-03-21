33575f76db1d8c95c144c1b33c0d8cf69f36768c
admin <ekene@Ubuntu22.04.myguest.virtualbox.org>
2023-03-21 13:32:14 +0100

    print sign of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_sign- Shows 1 if the integer is greater than 0, 0 if integer is 0 and -1 if integer is less than 0
+*
+* @n: The integer
+*
+* Return: 1 for letter character. 0 for the rest
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+_putchar('+');
+}
+else
+{
+if (n == 0)
+{
+return (0);
+_putchar('0');
+}
+else
+return (-1);
+	    
