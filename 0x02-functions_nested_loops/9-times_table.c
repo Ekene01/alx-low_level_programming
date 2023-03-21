e17007cf986dd13b4200fbf7c71f9a41b459a24a
admin <ekene@Ubuntu22.04.myguest.virtualbox.org>
2023-03-21 15:49:45 +0100

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@ void times_table(void)
 int i, j, k, l, m;
 for (i=0; i<=9; i++)
 {
-for (j=0; j<=9; y++)
+for (j=0; j<=9; j++)
 {
 k = j * i;
 if (k > 9)
@@ -18,12 +18,12 @@ l = k % 10;
 m = (k - l) / 10;
 _putchar(44);
 _putchar(32);
-_putchar(d + '0');
-_putchar(u + '0');
+_putchar(m + '0');
+_putchar(l + '0');
 }
 else
 {
-if (y != 0)
+if (j != 0)
 {
 _putchar(44);
 _putchar(32);
