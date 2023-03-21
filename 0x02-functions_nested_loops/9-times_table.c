d39731b8228a96f4123b8606066d348e238389f1
admin <ekene@Ubuntu22.04.myguest.virtualbox.org>
2023-03-21 15:47:39 +0100

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+* times_table- prints the 9 tiems table starting with 0
+*
+* Return: Always 0
+*/
+void times_table(void)
+{
+int i, j, k, l, m;
+for (i=0; i<=9; i++)
+{
+for (j=0; j<=9; y++)
+{
+k = j * i;
+if (k > 9)
+{
+l = k % 10;
+m = (k - l) / 10;
+_putchar(44);
+_putchar(32);
+_putchar(d + '0');
+_putchar(u + '0');
+}
+else
+{
+if (y != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+_putchar(l + '0');
+}
+}
+_putchar('\n');
+}
+}
