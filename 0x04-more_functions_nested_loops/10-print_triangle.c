ce69ed0d1ce02546e7d60c54b9e19f3adef1ea0a
admin <ekene@Ubuntu22.04.myguest.virtualbox.org>
2023-03-23 15:02:00 +0100

    more numbers

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,24 +6,25 @@
  */
 void print_triangle(int size)
 {
-int row, hashes, spaces;
-if (size <= 0)
-{
-_putchar('\n');
-}
-else
-{
-for (row = 0; row <= size; row++)
-{
-for (spaces = size - row; spaces >= 1; spaces--)
-{
-_putchar(' ');
-}
-for (hashes = 1; hashes <= row; hashes++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
-}
+	int row, hashes, spaces;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
