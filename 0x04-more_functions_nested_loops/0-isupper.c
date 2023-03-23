eeba89279033dc0a2368faea8288677f92c4ddb7
admin <ekene@Ubuntu22.04.myguest.virtualbox.org>
2023-03-23 08:52:52 +0100

    uppercase character function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* _isupper - Shows 1 if the input is an uppercase character else shows 0
+*
+* @c: The character in ASCII code
+*
+* Return: 1 for uppercase character. 0 for the rest
+*/
+int _islower(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}int _isupper(int c)
+{
+  
+}
+  
