9a597849bbe4977fd0a69d6ac8f05757d39e1f43
admin <ekene@Ubuntu22.04.myguest.virtualbox.org>
2023-03-23 13:38:41 +0100

    more numbers

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,11 +1,9 @@
 #include "main.h"
 /**
-* print_square - A program that prints a square, followed by a new line
-*
-* @size - This is the size of the square
-*
-* Return: 0 (Success)
-*/
+ * print_square - prints a square of size size
+ * @size: size of the square
+ * Return: void
+ */
 void print_square(int size)
 {
 int i;
