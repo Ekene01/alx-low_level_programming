53607c0ad94fe459f3b1275618a65575ab712e06
admin <ekene@Ubuntu22.04.myguest.virtualbox.org>
2023-03-23 14:52:47 +0100

    more numbers

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the square
+ * Return: void
+ */
+void print_triangle(int size)
+{
+int row, hashes, spaces;
+if (size <= 0)
+{
+_putchar('\n');
+}
+for (row = 0; row <= size; row++)
+{
+for (spaces = size - row; spaces >= 1; spaces--)
+{
+_putchar(' ');
+}
+for (hashes = 1; hashes <= row; hashes++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+
