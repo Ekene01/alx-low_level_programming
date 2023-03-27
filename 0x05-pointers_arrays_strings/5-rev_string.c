e308b34a66d6c32769bad64256a300895e2e5787
admin <ekene@Ubuntu22.04.myguest.virtualbox.org>
2023-03-27 16:39:28 +0100

    printing a string in reverse per word

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*** print_rev - prints a string in reverse
+* @s: String to be printed
+* Return: 0 (Success)
+*/
+void rev_string(char *s)
+{
+int len = 0, index = 0;
+char tmp;
+while (s[index++])
+len++;
+for (index = len - 1; index >= len /2; index--)
+{
+tmp = s [index];
+s[index] = s[len - index - 1];
+s[len - index - 1] = tmp;
+}
+}
