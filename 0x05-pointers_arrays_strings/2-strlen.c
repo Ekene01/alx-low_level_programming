6eb66844a1c9b7839be10ac0e5b50c9e66bdd1f7
admin <ekene@Ubuntu22.04.myguest.virtualbox.org>
2023-03-27 14:24:27 +0100

    changes

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-int _strlen(char *s)
-{
-  unsigned int count = 0;
-  while (*s != '\0')
-  {
-      count++;
-      s++;
-  }
-  return  count;
-=======
 #include "main.h"
 /**
 * _strlen - A function that returns the length of a string
@@ -26,5 +15,4 @@ count++;
 s++;
 }
 return (count);
->>>>>>> b3abb7ce7dce42798a0a5d4df82965468b38964c
 }
