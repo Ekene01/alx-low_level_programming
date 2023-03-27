df1c3623ecacfc63548cbc0fa08b17398762f53d
admin <ekene@Ubuntu22.04.myguest.virtualbox.org>
2023-03-27 15:16:24 +0100

    print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_rev - prints a string in reverse
+* @str: String to be printed
+* str - string to be printed
+* Return: 0 (Success)
+*/
+void print_rev(char *s)
+{
+int count = 0;
+while (s[count] != '\0')
+{
+count++;
+}
+for (count -= 1; count >= 0; count--)
+{
+_putchar(s[count]);
+}
+}
