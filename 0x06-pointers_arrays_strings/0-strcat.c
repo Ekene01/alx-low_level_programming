6ec3aa2268770310cb2786f85c3a4fa87f6377ad
admin <ekene@Ubuntu22.04.myguest.virtualbox.org>
2023-03-30 08:28:38 +0100

    finally done oh

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ *
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j = 0;
+while (dest[i++])
+{
+j++;
+}
+for (i = 0; src[i]; i++)
+{
+dest[j++] = src[i];
+}
+return (dest);
+}
