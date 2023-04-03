b3d593bad43a82840bf5a81cbd34f343010c62b3
Ekene01 <ekeneikoro@gmail.com>
2023-04-03 21:22:25 +0100

    updated

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _strspn - A function to get length of substring
- * @s: The string we are searching
- * @accept: The values we are searching for
- * Return: Number of bytes
+ * _strspn - Gets the length of a substring
+ * @s: The string to be searched
+ * @accept: The prefix to be measured
+ * Return: The number of bytes
  */
 unsigned int _strspn(char *s, char *accept)
 {
@@ -11,19 +11,20 @@ unsigned int _strspn(char *s, char *accept)
 	int i;
 	while (*s)
 	{
-		for(i=0; accept[i]; i++)
+		for (i = 0; accept[i]; i++)
 		{
-			if (*s == accept[i])
+			if(*s == accept[i])
 			{
 				length++;
 				break;
 			}
 			else if (accept[i + 1] == '\0')
 			{
-				return(length);
+				return (length);
 			}
 			s++;
 		}
 		return (length);
 	}
 }
+
