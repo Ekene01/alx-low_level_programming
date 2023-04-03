737256fded2508f01325d9c1e51651a6433c95f9
Ekene Ikoro <Ekene01@users.noreply.github.com>
2023-04-03 21:44:35 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,11 +9,12 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int length = 0;
 	int i;
+	
 	while (*s)
 	{
 		for (i = 0; accept[i]; i++)
 		{
-			if(*s == accept[i])
+			if (*s == accept[i])
 			{
 				length++;
 				break;
